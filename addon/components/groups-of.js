c39f5cbbe5cc864239962d81a0e7a4b2f995fbaf
J Corley <jfcorley+github@gmail.com>
2016-03-14 11:28:09 -0500

    added support for remainders

--- addon/components/groups-of.js
+++ addon/components/groups-of.js
@@ -11,7 +11,10 @@ export default Ember.Component.extend({
 
     items: Ember.A(),
 
+    remainderItems: Ember.A(),
+
     groups: Ember.computed(
+        'groupsOf',
         'items',
         function() {
             const items = this.get( 'items' );
@@ -27,7 +30,15 @@ export default Ember.Component.extend({
             items.forEach( function( item, index ) {
                 if ( !(index % groupsOf) ) {
                     currentGroup = Ember.A();
-                    groups.push( currentGroup );
+
+                    const isFirstGroup = !!groups.length;
+                    const isLastGroup = groups.length == parseInt( items.length / groupsOf );
+
+                    groups.push({
+                        items: currentGroup,
+                        isFirstGroup,
+                        isLastGroup
+                    });
                 }
 
                 const isFirst = !currentGroup.length;
@@ -41,6 +52,31 @@ export default Ember.Component.extend({
             });
 
             return groups;
+        }
+    ),
+
+    remainders: Ember.computed(
+        'groupsOf',
+        'items',
+        function() {
+            const items = this.get( 'items' );
+            const groupsOf = this.get( 'groupsOf' );
+
+            if ( !groupsOf ) {
+                return;
+            }
+
+            const remainder = Ember.A();
+            const remainderCount = groupsOf - ( items.length % groupsOf );
+
+            for ( let item = 0; item < remainderCount; item++ ) {
+                remainder.push({
+                    item,
+                    isLast: item == remainderCount - 1
+                });
+            }
+
+            return remainder;
         }
     )
 
--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-    tableData: [ 1, 2, 3, 4, 5, 6 ]
+    tableData: [ 1, 2, 3, 4, 5, 6, 7 ]
 
 });
